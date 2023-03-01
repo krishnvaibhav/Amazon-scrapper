705bb97732ea5d921282db2e57cf73c31ab018bc
krishnvaibhav <krishnaibhav.12c1@gmail.com>
2023-03-01 14:23:03 +0530

    project done

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import smtplib
+
+link = 'https://www.amazon.com/dp/B00X9JNWGS/ref=sbl_dpx_kitchen-electric-cookware_B08GC6PL3D_0'
+
+header = {
+    'Accept-Language': 'en-US,en;q=0.9',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36 '
+}
+
+response = requests.get(link,headers=header)
+
+soup = BeautifulSoup(response.text,'lxml')
+
+item = soup.select_one(selector=".a-price.a-text-price.a-size-medium.apexPriceToPay .a-offscreen")
+
+price = float(item.getText().split("$")[1])
+
+
+target_price = 550
+
+if price < target_price :
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+        connection.starttls()
+        connection.login(user="anonymousrobot974@gmail.com",password="robot@974")
+        connection.sendmail(from_addr="anonymousrobot974@gmail.com",to_addrs="krishnvaibhav.12c1@gmail.com",
+                            msg=f"subject:Price target achieved \n\n the price is currently at {price} quickly purchase it")
